5a866e1b34fb6bf4465367586e68ed9af68251f3
Ori Blanka <oriblanka305@gmail.com>
2023-06-27 23:12:50 +0300

    Final commit for EX6

--- Exercise6/src/hw6.js
+++ Exercise6/src/hw6.js
@@ -23,13 +23,11 @@ function GetCard(cardStrac) {
     return new THREE.Mesh(cardGeometry, cardMaterial);
 }
 
-function GetPointsForRoute(routeVector, numberOfPointes) {
-    return new THREE.QuadraticBezierCurve3(
-        ball.position, routeVector, new THREE.Vector3(0, -3, -4)
-    ).getPoints(numberOfPointes);
+function GetPointsForRoute(routeVector, pointsNumber) {
+    return new THREE.QuadraticBezierCurve3(ball.position, routeVector, new THREE.Vector3(0, -3, -4)).getPoints(pointsNumber);
 }
 
-let allCards = new Map();
+let cards = new Map();
 
 function createCards(cardsIndexes, cardType, routePoints, route) {
     cardsIndexes.forEach(index => {
@@ -37,7 +35,7 @@ function createCards(cardsIndexes, cardType, routePoints, route) {
         const point = routePoints[index]
         card.position.set(point.x, point.y, point.z)
         scene.add(card)
-        allCards.set(index, new Card(point, cardType, route, card))
+        cards.set(index, new Card(point, cardType, route, card))
     })
 }
 
@@ -98,8 +96,8 @@ const cylinderMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, side: THR
 // Create net's left side
 const leftGoalPost = new THREE.Mesh(cylinderGeometryF, cylinderMaterial);
 const leftGoalPostB = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-leftGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(15, 0, 7));
-leftGoalPostB.applyMatrix4(new THREE.Matrix4().makeTranslation(15, 0, 0));
+leftGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(15, 0, 0));
+leftGoalPostB.applyMatrix4(new THREE.Matrix4().makeTranslation(15, 0, -7));
 leftGoalPostB.rotateX(Math.PI / 4);
 
 groupGoal.add(leftGoalPost);
@@ -108,8 +106,8 @@ groupGoal.add(leftGoalPostB);
 // Create net's right side
 const rightGoalPost = new THREE.Mesh(cylinderGeometryF, cylinderMaterial);
 const rightGoalPostB = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-rightGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, 0, 7));
-rightGoalPostB.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, 0, 0));
+rightGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, 0, 0));
+rightGoalPostB.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, 0, -7));
 rightGoalPostB.rotateX(Math.PI / 4);
 
 groupGoal.add(rightGoalPost);
@@ -117,7 +115,7 @@ groupGoal.add(rightGoalPostB);
 
 // Create net's top side
 const topGoalPost = new THREE.Mesh(cylinderGeometryT, cylinderMaterial);
-topGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 7, 7));
+topGoalPost.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 7, 0));
 
 topGoalPost.rotateZ(-Math.PI / 2);
 groupGoal.add(topGoalPost);
@@ -127,22 +125,22 @@ const rings = new THREE.CircleGeometry(1, 60);
 const ring1 = new THREE.Mesh(rings, cylinderMaterial);
 groupGoal.add(ring1);
 ring1.rotateX(-Math.PI / 2)
-ring1.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -7, 7));
+ring1.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -7, 0));
 
 const ring2 = new THREE.Mesh(rings, cylinderMaterial);
 groupGoal.add(ring2);
 ring2.rotateX(-Math.PI / 2)
-ring2.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -7, 7));
+ring2.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -7, 0));
 
 const ring3 = new THREE.Mesh(rings, cylinderMaterial);
 groupGoal.add(ring3);
 ring3.rotateX(-Math.PI / 2)
-ring3.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -7, -6.5));
+ring3.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -7, -13.5));
 
 const ring4 = new THREE.Mesh(rings, cylinderMaterial);
 groupGoal.add(ring4);
 ring4.rotateX(-Math.PI / 2)
-ring4.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -7, -6.5));
+ring4.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -7, -13.5));
 
 // Nets creating
 const indices = [0, 1, 2]
@@ -151,8 +149,14 @@ const vertices = [
     -12, -4, 0,
     2, 10, 0];
 
-const netBackSkeleton = new THREE.MeshBasicMaterial({color: 0x808080, side: THREE.DoubleSide, transparent: true, opacity: 0.7});
+const netBackSkeleton = new THREE.MeshBasicMaterial({
+    color: 0x808080,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 0.7
+});
 const netBack = new THREE.Mesh(new THREE.PlaneGeometry(31, 19), netBackSkeleton);
+netBack.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, -7))
 netBack.rotateX(Math.PI / 4)
 groupGoal.add(netBack);
 
@@ -162,7 +166,7 @@ rightNetGeometry.setIndex(indices)
 rightNetGeometry.computeVertexNormals()
 const netLeft = new THREE.Mesh(rightNetGeometry, netBackSkeleton);
 netLeft.rotateY(-Math.PI / 2)
-netLeft.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -3, 5))
+netLeft.applyMatrix4(new THREE.Matrix4().makeTranslation(-15, -3, -2))
 groupGoal.add(netLeft)
 
 const leftNetGeometry = new THREE.BufferGeometry()
@@ -172,7 +176,7 @@ leftNetGeometry.computeVertexNormals()
 leftNetGeometry.computeVertexNormals()
 const netRight = new THREE.Mesh(leftNetGeometry, netBackSkeleton);
 netRight.rotateY(-Math.PI / 2)
-netRight.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -3, 5))
+netRight.applyMatrix4(new THREE.Matrix4().makeTranslation(15, -3, -2))
 groupGoal.add(netRight)
 
 scene.add(groupGoal);
@@ -186,66 +190,47 @@ ball.position.set(0, 0, 100)
 scene.add(ball);
 
 // TODO: Bezier Curves
-const numberOfPointes = 5000; // Number of points to generate
-const pointsRight = GetPointsForRoute(new THREE.Vector3(50, 0, 50), numberOfPointes);
-const pointsLeftenter = GetPointsForRoute(new THREE.Vector3(0, 50, 50), numberOfPointes);
-const pointsLeft = GetPointsForRoute(new THREE.Vector3(-50, 0, 50), numberOfPointes);
-const routes_list = [pointsRight, pointsLeftenter, pointsLeft]
+const pointsNumber = 5000; // Number of points to generate
+const pointsRight = GetPointsForRoute(new THREE.Vector3(50, 0, 50), pointsNumber);
+const pointsLeftCenter = GetPointsForRoute(new THREE.Vector3(0, 50, 50), pointsNumber);
+const pointsLeft = GetPointsForRoute(new THREE.Vector3(-50, 0, 50), pointsNumber);
+const routes_list = [pointsRight, pointsLeftCenter, pointsLeft]
 
 // TODO: Camera Settings
 // Set the camera following the ball here
 const cameraTranslate = new THREE.Matrix4();
 cameraTranslate.makeTranslation(-2, 8, 10);
 camera.applyMatrix4(cameraTranslate)
-camera.rotateX(THREE.MathUtils.degToRad(-15))
-
-// TODO: I write this line in order to test objects' locations
-camera.applyMatrix4((new THREE.Matrix4().makeRotationY(degrees_to_radians(0))));
 
 // TODO: Add collectible cards with textures
-// TODO: Cards hasn't color - Needs to be fixed!
 const card = GetCard(yellowcardtexture);
 console.log(card);
-card.position.set(4, 20, 75);
+// card.position.set(10, 30, 75);
 console.log(card);
-const routeACardsRed = [600, 1800, 950]
-const routeACardsYellow = [3820, 2950]
+const routeACardsRed = [550, 1900, 800, 400]
+const routeACardsYellow = [1050, 1400, 3800, 3000]
 createCardsForRoute(routeACardsRed, routeACardsYellow, pointsRight, 0)
-const routeBCardsRed = [2800, 3600]
-const routeBCardsYellow = [825, 1100]
-createCardsForRoute(routeBCardsRed, routeBCardsYellow, pointsLeftenter, 1)
-const routeCCardsRed = [425, 1900, 4500]
-const routeCCardsYellow = [1000, 3200]
+const routeBCardsRed = [1000, 2000, 2800, 3600]
+const routeBCardsYellow = [825, 1100, 2300, 4000]
+createCardsForRoute(routeBCardsRed, routeBCardsYellow, pointsLeftCenter, 1)
+const routeCCardsRed = [1900, 3500]
+const routeCCardsYellow = [1000, 3200, 4000]
 createCardsForRoute(routeCCardsRed, routeCCardsYellow, pointsLeft, 2)
 
-
-// TODO: Add keyboard event
-// We wrote some of the function for you
-let startGame = false
-let currentRoute = 1;
-let currentIndex = 1;
-
-const handle_keydown = (e) => {
-    if (e.code == 'ArrowLeft') {
-        currentRoute = (currentRoute + 1) % routes_list.length;
-    }
-
-    if (e.code === 'ArrowRight') {
-        currentRoute = (currentRoute - 1 + routes_list.length) % routes_list.length;
-    }
-
-    if (e.code === 'ArrowUp') {
-        startGame = true;
-    }
-}
-document.addEventListener('keydown', handle_keydown);
+// Bonus! Add the flag of our favorite club
+const brazilFlagTexture = new THREE.TextureLoader().load('src/textures/football_brazil.jpg');
+const brazilFlagMaterial = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: brazilFlagTexture });
+const flagGeometry = new THREE.PlaneGeometry(20, 10)
+const flag = new THREE.Mesh(flagGeometry, brazilFlagMaterial);
+flag.position.set(0, 15, 0);
+scene.add(flag)
 
 let redCardCounter = 0;
 let yellowCardCounter = 0;
 
-function checkHits() {
-    if (allCards.has(currentRoute) && allCards.get(currentRoute).route == currentIndex) {
-        const hitCard = allCards.get(currentRoute);
+function checkCardsHits() {
+    if (cards.has(currentRoute) && cards.get(currentRoute).route == currentIndex) {
+        const hitCard = cards.get(currentRoute);
         hitCard.cardObject.visible = false;
         hitCard.cardType == yellowcardtexture ? yellowCardCounter++ : redCardCounter++;
     }
@@ -257,21 +242,82 @@ function calcScore() {
     const totalScore = 100 * 2 ** (-((yellow + red) / 10));
 
     return totalScore;
-
 }
+
+// TODO: Add keyboard event
+// We wrote some of the function for you
+let startGame = false
+let currentRoute = 1;
+let currentIndex = 1;
+let timer;
+let startTime;
+let paused = true;
+let ballSpeed = 0.08;
+
+const handle_keydown = (e) => {
+    if (e.code === "ArrowLeft") {
+        if (currentIndex < 2) {
+            currentIndex = currentIndex + 1;
+        }
+    } else if (e.code === "ArrowRight") {
+        if (currentIndex > 0) {
+            currentIndex = currentIndex - 1;
+        }
+    } else if (e.code === "ArrowUp") {
+        ballSpeed = ballSpeed + 0.1;
+    } else if (e.code === "ArrowDown") {
+        ballSpeed = ballSpeed - 0.1; // Decrease ball speed
+        if (ballSpeed < 0) {
+            ballSpeed = 0; // Ensure ball speed is not negative
+        }
+    } else if (e.code === "Enter") {
+        startGame = !startGame; // Toggle startGame state
+        if (startGame && paused) {
+            startTime = Date.now(); // Start the timer
+            paused = false;
+            timer = setInterval(() => {
+                const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+                if (elapsedTime >= 50) {
+                    alert("Time's up! Try again!");
+                    clearInterval(timer);
+                    location.reload();
+                }
+            }, 1000);
+        } else {
+            paused = true; // Pause the timer
+            clearInterval(timer);
+        }
+    }
+    const point = routes_list[currentIndex][currentRoute]
+    ball.position.set(point.x, point.y, point.z);
+}
+document.addEventListener('keydown', handle_keydown);
+
 function animate() {
-
     requestAnimationFrame(animate);
-
     // Update ball's position along the current route
-    if (currentIndex < numberOfPointes - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 1;
+    if (startGame) {
+        if (currentRoute < routes_list[currentIndex].length - 1) {
+            const point = routes_list[currentIndex][currentRoute]
+            ball.position.set(point.x, point.y, point.z);
+            camera.position.set(point.x, point.y + 10, point.z + 20);
+            currentRoute = currentRoute + 1;
+        }
     }
 
-    const currentPoint = routes_list[currentRoute][currentIndex];
-    ball.position.copy(currentPoint);
+    checkCardsHits();
+    if (currentRoute == routes_list[currentIndex].length - 1) {
+        currentRoute = currentRoute + 1;
+        const score = calcScore()
+        const message = `You hit ${redCardCounter} red and ${yellowCardCounter} yellow cards \n your score is: ${score}\n
+			${(score >= 90) ? "Great job!! 🤩🥳" : "Needs to improve! Try again! 🙂"}`
+        alert(message);
+        location.reload();
+    }
+
+    if (currentRoute > 1) {
+        ball.rotation.y += ballSpeed;
+    }
 
     // Rotate ball around itself
     ball.rotation.x += 0.025;
@@ -279,5 +325,6 @@ function animate() {
     renderer.render(scene, camera);
 
 }
-
+alert("For start press Enter 🙂 If you want to pause ball's move press Enter again!\n" +
+    "You have 50 seconds to finish! GOOD LUCK!! 🤩")
 animate()
